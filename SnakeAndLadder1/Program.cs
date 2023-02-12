0fba154588eecdeb429fdd0ac4266dc547d1f6fd
guruprasath <guruprasath1051@gmail.com>
2023-02-12 20:24:45 +0530

    Adding checking position

--- SnakeAndLadder1/Program.cs
+++ SnakeAndLadder1/Program.cs
@@ -8,14 +8,43 @@ namespace SnakeAndLadder1
 {
     internal class Snake
     {
+        public const int ladder = 1, snake = 2;
         public void Ladder()
         {
-            int position = 0;
+            int position1 = 1;
             Console.WriteLine("The position of player is : " + position);
             Random random = new Random();
             int dice = random.Next(1, 7);
             Console.WriteLine("The number got by dice roll is : " + dice);
-            
+            Random random2 = new Random();
+            int option = random2.Next(0,3);
+            switch (option)
+            {
+                case ladder:
+                    position1 += dice;
+                    Console.WriteLine("The player climbed the ladder");
+                    break;
+                case snake:
+                    position1 += dice;
+                    if (position1 < 0)
+                    {
+                        Console.WriteLine("The player bit by snake");
+                        position1 = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The player bit by snake");
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"NO play");
+                    break;
+            }
+            Console.WriteLine("The player position is :" + position1);
+            Console.WriteLine(" ");
+                        
+
+            }
         }
     }
 }
