9e2089afac3518031412da3a496433809334cd06
guruprasath <guruprasath1051@gmail.com>
2023-02-12 20:38:23 +0530

    Adding winning position

--- SnakeAndLadder1/Program.cs
+++ SnakeAndLadder1/Program.cs
@@ -11,40 +11,42 @@ namespace SnakeAndLadder1
         public const int ladder = 1, snake = 2;
         public void Ladder()
         {
-            int position1 = 1;
-            Console.WriteLine("The position of player is : " + position);
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            Console.WriteLine("The number got by dice roll is : " + dice);
-            Random random2 = new Random();
-            int option = random2.Next(0,3);
-            switch (option)
-            {
-                case ladder:
-                    position1 += dice;
-                    Console.WriteLine("The player climbed the ladder");
-                    break;
-                case snake:
-                    position1 += dice;
-                    if (position1 < 0)
-                    {
-                        Console.WriteLine("The player bit by snake");
-                        position1 = 0;
-                    }
-                    else
-                    {
-                        Console.WriteLine("The player bit by snake");
-                    }
-                    break;
-                default:
-                    Console.WriteLine($"NO play");
-                    break;
-            }
-            Console.WriteLine("The player position is :" + position1);
+            int position1 = 0;
+            Console.WriteLine("The position of player is : " + position1);
             Console.WriteLine(" ");
-                        
+            do
+            {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("The number got by dice roll is : " + dice);
+                Random random2 = new Random();
+                int option = random2.Next(0, 3);
+                switch (option)
+                {
+                    case ladder:
+                        position1 += dice;
+                        Console.WriteLine("The player climbed the ladder");
+                        break;
+                    case snake:
+                        position1 += dice;
+                        if (position1 < 0)
+                        {
+                            Console.WriteLine("The player bit by snake");
+                            position1 = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The player bit by snake");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine($"NO play");
+                        break;
+                }
+                Console.WriteLine("The player position is :" + position1);
+                Console.WriteLine(" ");
 
-            }
+            } while (position1 < 100);           
         }
     }
 }
